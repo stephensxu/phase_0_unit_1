43f4d5b62ae58bba08f4613d9ccd30aa1710a0e3
Stephens Xu <stephensxub@gmail.com>
2014-05-27 15:51:35 -0700

    added solution for 3/4

--- week_3/4_manipulating_js_objects/my_solution.js
+++ week_3/4_manipulating_js_objects/my_solution.js
@@ -17,25 +17,62 @@ var terah = {
 // __________________________________________
 // Write your code below.
 
+// Pseudocode:
 
+// Give adam a name property with the value "Adam".
 
+// Add a spouse property to terah and assign it the value of adam.
 
+// Change the value of the terah weight property to 125.
 
+// Remove the eyeColor property from terah.
 
+// Add a spouse property to adam and assign it the value of terah.
 
+// Add a children property to terah and and use object literal notation to assign this variable the value of a JavaScript Object object with no properties
+
+// Add a carson property to the value of the terah children property and assign it the value of an object with the property name with a value of "Carson".
+
+// Add a carter property to the value of the terah children property and assign it the value of an object with the property name with a value of "Carter".
+
+// Add a colton property to the value of the terah children property and assign it the value of an object with the property name with a value of "Colton".
+
+// Add a children property to adam and assign it the value of terah children.
+
+var adam = {
+  name: "Adam",
+}
+
+terah.spouse = adam
+terah.weight = 125
+delete terah.eyeColor
+terah.spouse.spouse = terah
+terah.children = {}
+terah.children.carson = {}
+terah.children.carson.name = "Carson"
+terah.children.carter = {}
+terah.children.carter.name = "Carter"
+terah.children.colton = {}
+terah.children.colton.name = "Colton"
+adam.children = terah.children
 
 // __________________________________________
 // Reflection: Use the reflection guidelines
-// 
-// 
-// 
-// 
-// 
-// 
+// My stategy is to read through all the testing parameters frist before I start coding. This gives me an idea of 
+// the bigger picture of what I'm trying to solve. This appraoch can potentially save me some time as I develope
+// because some testing parameter are overlaped on one single object. 
+// During the process I had question about how to add "carson" as an object to "children" while the "children" object
+// doesn't exist yet. At first, I tried to make "Carson" as a seperate variable, then "children" as an array object property 
+// of terah, and pushing "Carson" to the "Children" array. However this approach didn't pass the test. Later on I realized
+// that I have to use object literal to make the "children" property first, then make the "carson" property, then make the name 
+// property and add a string to the "name" property. I am confident with each of the Learning Competencies. I enjoyed the part
+// where I have to think, read through the psuedo code and find the right solution. When I'm stuck I get a little frustrated.
+//
 
 
 // __________________________________________
 // Driver Code:  Do not alter code below this line.
+// function assert(test, message, test_number) {
 function assert(test, message, test_number) {
   if (!test) {
     console.log(test_number + "false");
